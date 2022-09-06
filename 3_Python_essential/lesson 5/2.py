63ca7c97f60d6e7cc74f11b5cfc150a09e6f7200
shmelneg <shmelneg@gmail.com>
2022-09-06 23:39:35 +0300

    hw4

---
+++ 3_Python_essential/lesson 5/2.py
@@ -0,0 +1,39 @@
+class Contact:
+
+    def __init__(self, surname="Doe", name="John", age=33, mob_phone="095-555-55-55", email="test@test.com"):
+        self.surname = surname
+        self.name = name
+        self.age = age
+        self.mob_phone = mob_phone
+        self.email = email
+
+    def get_contact(self):
+        return f"{self.name} {self.surname} - {self.mob_phone}"
+
+    def send_message(self):
+        print(f"Sending message to {self.name} {self.surname} at {self.email}")
+
+
+class UpdateContact(Contact):
+
+    def __init__(self, surname="Doe", name="Jane", age=25, mob_phone="095-111-11-11", email="test2@test.com", job=None):
+        super().__init__(surname, name, age, mob_phone, email)
+        self.job = job
+
+    def get_message(self):
+        print(f"{self.name}, you've got an email at {self.email}. Do you want to open it?")
+
+
+contact1 = Contact()
+contact2 = UpdateContact(job="EPAM")
+
+print(Contact.__dict__)
+print(contact1.__dict__)
+print(UpdateContact.__dict__)
+print(contact2.__dict__)
+print()
+print(Contact.__base__)
+print(UpdateContact.__base__)
+print()
+print(Contact.__bases__)
+print(UpdateContact.__bases__)
---
+++ 3_Python_essential/lesson 5/3.py
@@ -0,0 +1,39 @@
+class Contact:
+
+    def __init__(self, surname="Doe", name="John", age=33, mob_phone="095-555-55-55", email="test@test.com"):
+        self.surname = surname
+        self.name = name
+        self.age = age
+        self.mob_phone = mob_phone
+        self.email = email
+
+    def get_contact(self):
+        return f"{self.name} {self.surname} - {self.mob_phone}"
+
+    def send_message(self):
+        print(f"Sending message to {self.name} {self.surname} at {self.email}")
+
+
+class UpdateContact(Contact):
+
+    def __init__(self, surname="Doe", name="Jane", age=25, mob_phone="095-111-11-11", email="test2@test.com", job=None):
+        super().__init__(surname, name, age, mob_phone, email)
+        self.job = job
+
+    def get_message(self):
+        print(f"{self.name}, you've got an email at {self.email}. Do you want to open it?")
+
+contact1 = Contact()
+contact2 = UpdateContact()
+
+print(hasattr(contact1, "job"))
+print(hasattr(contact2, "job"))
+print()
+print(getattr(contact1, "name"))
+print(getattr(contact2, "name"))
+print()
+setattr(contact1, 'name', 'Ivan')
+print(getattr(contact1, "name"))
+print()
+delattr(contact2, "job")
+print(hasattr(contact2, "job"))
