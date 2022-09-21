e83f0374317dd71834bdcddd5f658e5fc05c1c7b
andriimelnyk <shmelneg@gmail.com>
2022-09-21 11:10:40 +0300

    advanced lesson 1

--- 4_Python_advanced/lesson 1/2.py
+++ 4_Python_advanced/lesson 1/2.py
@@ -3,14 +3,18 @@ start, finish = map(int, answer.split())
 first_list = list(range(start, finish + 1))
 print(f"The list of integers if the following: {first_list}")
 
+# solution via list generator
 second_list = [x**2 for x in first_list if x % 2 != 0]
 print(f"The list of squared values of the odd numbers if the following: {second_list}")
 
-# the same but with lambda, map and filter
+# solution via lambda, map and filter
+
 # filter odd numbers to a new list
 odd_list = list(filter(lambda x: x % 2 != 0, first_list))
 
+# creating short-name function for squared values
 sqr = lambda x: x**2
 
+# applying sqr to the odd list
 third_list = list(map(sqr, odd_list))
 print(f"The the same as the 2nd list but with lambda, map and filter: {third_list}")
